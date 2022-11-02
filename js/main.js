f0dd45819a1e2f42bd78fc0563c88ef15b000442
Tetiankaa <tanyakorsun0208@gmail.com>
2022-11-02 21:34:23 +0100

    some text

---
+++ js/main.js
@@ -0,0 +1,51 @@
+let a = `hello`
+console.log(a)
+
+let b = `owu`
+console.log(b)
+
+let c = `com`
+console.log(c)
+
+let d = `ua`
+console.log(d)
+
+let number = 1
+console.log(number)
+
+let number10 = 10
+console.log(number10)
+
+console.log(-1000+1)
+
+let number123 = 123
+console.log(number123)
+
+const PI = 3.14
+console.log(PI)
+
+let float = 2.7
+console.log(float)
+
+let number16 = 16
+console.log(number16)
+
+console.log(10 > 5)
+
+console.log(10 !== 10)
+
+let firstName = `Tetiana`
+let middleName = `Borysivna`
+let lastName = `Korsun`
+
+let msg = `${firstName} ${middleName} ${lastName}`
+console.log(msg)
+
+a = 100
+console.log(typeof a)
+
+b = 100
+console.log(typeof b)
+
+c = true
+console.log(typeof c)
