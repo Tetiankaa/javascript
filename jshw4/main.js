baf92642a7c3153b78be3955d253fe4b06e45630
Tetiankaa <tanyakorsun0208@gmail.com>
2022-11-11 00:12:28 +0100

    some text / hw4

---
+++ jshw4/main.js
@@ -0,0 +1,138 @@
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+for (let i=0;i<10;i++) {
+    document.write(`<div><h3>hello</h3></div>`);
+}
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+for (let i=0;i<10;i++) {
+    document.write(`<div><h3>${i}hello</h3></div>`);
+}
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+let i = 0;
+while (i<20){
+console.log(`<h1>block</h1>`)
+    i++
+}
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+let a = 0;
+while (a<20){
+    console.log(`<h1>${a}block</h1>`)
+    a++
+}
+// - Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+for (const listOfItem of listOfItems) {
+    document.write(`
+<ul>
+<li>${listOfItem}</li>
+</ul>
+`)
+}
+// Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону  Зробити адекватну стилізацію
+// Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
+for (const product of products) {
+    document.write(`
+<div>
+<h3>${product.title}, ${product.price} eur</h3>
+<img src="${product.image}" alt="">
+</div>
+`)
+}
+
+// за допомоги циклу вивести:
+
+//     - користувачів зі статусом true
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+for (let i = 0; i<users.length; i++){
+
+    const user = users[i];
+    if (user.status){
+        document.write(`<div>
+    <p class="one">${user.name} ${user.age} ${user.status}</p>
+</div>`)
+    }
+}
+// - користувачів зі статусом false
+let userss = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+for (let i =0; i < userss.length; i++){
+    const user = userss[i];
+    if (!user.status){
+        document.write(`<div>
+    <p class="two">${user.name} ${user.age} ${user.status}</p>
+ </div>`)
+    }
+}
+
+// - користувачів які старші за 30 років
+
+let usersss = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+for (let i = 0; i < usersss.length; i++){
+    const user = usersss[i];
+    if (user.age > 30){
+        document.write(`<div>
+<p class="three">${user.name} ${user.age} ${user.status}</p>
+</div>`)
+    }
+}
